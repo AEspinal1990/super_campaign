c9e9b025dc98d8408020f30b26c52bcbf45ccce7
Alex Espinal <aespinal2800@gmail.com>
2018-10-09 16:37:19 -0700

    Add POST /adduser to admin routes, error occurring when attempting to save a user

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,7 +1,24 @@
-import { Request, Response } from 'express';
-import { Repository } from 'typeorm';
+import { Request, Response }    from 'express';
+import { createConnection }     from "typeorm";
+import { CampaignManager }      from '../backend/entity/CampaignManager'  
 
 // This is just a test. Home should be what page? Under
 export const home = (req: Request, res: Response) => {
     res.send('Hello!');
 };
+
+export const addUser = async (req: Request, res: Response) => {
+    const user = new CampaignManager(3,[1,2,3]);
+    console.log('test')
+    createConnection()
+        .then(async connection => {
+            await connection.manager.save(user).then(() =>{
+                console.log('saved campaign manager?');
+            })
+            res.status(200).send(user);
+        })
+        .catch(e => {
+            console.log('ERROR:',e);
+            res.status(400).send(user);
+        });
+ };
