68ef5f77e675b2d0d3b704f734124beaf10714f0
Alex <alexander.doo@stonybrook.edu>
2018-11-03 05:12:03 -0400

    Created Post for view-task

--- src/routes/canvasser.ts
+++ src/routes/canvasser.ts
@@ -6,6 +6,7 @@ import { Availability } from '../backend/entity/Availability';
 import { Results } from '..//backend/entity/Results';
 import { CompletedLocation } from '../backend/entity/CompletedLocation';
 import { managerRouter } from './manager';
+import { io } from '../server';
 
 const router: Router = Router();
 
@@ -21,7 +22,7 @@ const isAuthenticated = (req, res, next) => {
     }
 }
 
-router.get('/calendar', async(req: Request, res: Response) => {
+router.get('/calendar', async (req: Request, res: Response) => {
     res.render('edit-availability');
 
 });
@@ -29,14 +30,14 @@ router.get('/calendar', async(req: Request, res: Response) => {
 /**
  * GET and POST for Edit Availability
  */
-router.get('/availability/:id', isAuthenticated, async(req: Request, res: Response) => {
+router.get('/availability/:id', isAuthenticated, async (req: Request, res: Response) => {
     const canvas = await getManager()
-       .createQueryBuilder(Canvasser, "canvasser")
-       .leftJoinAndSelect("canvasser._ID", "user")
-       .leftJoinAndSelect("canvasser._availableDates", "avaDate")
-       .leftJoinAndSelect("canvasser._assignedDates", "assDate")
-       .where("canvasser._ID = :ID", { ID: req.params.id })
-       .getOne();
+        .createQueryBuilder(Canvasser, "canvasser")
+        .leftJoinAndSelect("canvasser._ID", "user")
+        .leftJoinAndSelect("canvasser._availableDates", "avaDate")
+        .leftJoinAndSelect("canvasser._assignedDates", "assDate")
+        .where("canvasser._ID = :ID", { ID: req.params.id })
+        .getOne();
     console.log(canvas);
     if (canvas == undefined) {
         res.send("Wrong Link (Canvasser ID)");
@@ -59,7 +60,7 @@ router.get('/availability/:id', isAuthenticated, async(req: Request, res: Respon
             availableOrAssigned += curDateStr;
         }
         // if available dates is empty, then the first comma is not needed in the second for loop
-        if(canvas.availableDates == []) {
+        if (canvas.availableDates == []) {
             availableOrAssigned = availableOrAssigned.slice(1);
         }
         //res.send('You want to edit your availablity.');
@@ -70,27 +71,27 @@ router.get('/availability/:id', isAuthenticated, async(req: Request, res: Respon
     }
 });
 
-router.post('/availability/:id', isAuthenticated, async(req: Request, res: Response) => {
+router.post('/availability/:id', isAuthenticated, async (req: Request, res: Response) => {
     //new dates passed in from frontend
-    if (req.body.editAvailability.dates === ''){
+    if (req.body.editAvailability.dates === '') {
         return;
     }
     var newDates = req.body.editAvailability.dates.split(",");
     const canv = await getManager()
-       .createQueryBuilder(Canvasser, "canvasser")
-       .leftJoinAndSelect("canvasser._ID", "user")
-       .leftJoinAndSelect("canvasser._availableDates", "avaDate")
-       .leftJoinAndSelect("canvasser._assignedDates", "assDate")
-       .where("canvasser._ID = :ID", { ID: req.params.id })
-       .getOne();
+        .createQueryBuilder(Canvasser, "canvasser")
+        .leftJoinAndSelect("canvasser._ID", "user")
+        .leftJoinAndSelect("canvasser._availableDates", "avaDate")
+        .leftJoinAndSelect("canvasser._assignedDates", "assDate")
+        .where("canvasser._ID = :ID", { ID: req.params.id })
+        .getOne();
     // copy canvasser's old available dates
     var availCopy = [];
-    while(canv.availableDates.length > 0){
-        availCopy.push(canv.availableDates.splice(0,1)[0]);
+    while (canv.availableDates.length > 0) {
+        availCopy.push(canv.availableDates.splice(0, 1)[0]);
     }
     // copy canvasser's new available dates
     canv.availableDates = [];
-    for (let i in newDates){
+    for (let i in newDates) {
         var avail = new Availability();
         let newDateParts = newDates[i].split("/");
         if (newDateParts != null) {
@@ -100,17 +101,17 @@ router.post('/availability/:id', isAuthenticated, async(req: Request, res: Respo
         }
     }
     // delete old available dates that are unused
-    for (let i in availCopy){
-        for (let j in canv.availableDates){
-            if (+availCopy[i].availableDate === +canv.availableDates[j].availableDate){
+    for (let i in availCopy) {
+        for (let j in canv.availableDates) {
+            if (+availCopy[i].availableDate === +canv.availableDates[j].availableDate) {
                 canv.availableDates[j].ID = availCopy[i].ID;
                 break;
             }
-            if (Number(j) === canv.availableDates.length-1){
+            if (Number(j) === canv.availableDates.length - 1) {
                 await getRepository(Availability)
                     .createQueryBuilder()
                     .delete()
-                    .where("_ID = :ID", {ID: availCopy[i].ID})
+                    .where("_ID = :ID", { ID: availCopy[i].ID })
                     .execute();
             }
         }
@@ -135,32 +136,56 @@ router.post('/availability/:id', isAuthenticated, async(req: Request, res: Respo
 router.get('/:id/view-tasks', isAuthenticated, async (req: Request, res: Response) => {
     const canv = await getManager()
         .createQueryBuilder(Canvasser, "canvasser")
-        .leftJoinAndSelect("canvasser._campaigns", "campaign")
-        .leftJoinAndSelect("campaign._locations", "campLocations")
-        .leftJoinAndSelect("canvasser._ID", "user")
-        .leftJoinAndSelect("canvasser._availableDates", "avaDate")
-        .leftJoinAndSelect("canvasser._assignedDates", "assDate")
+        .leftJoinAndSelect("canvasser._task", "task")
+        .where("campaign._ID = :ID", { ID: req.params.id })
+        .getOne();
+    console.log("Canvasser: ", canv);
+
+    if (canv === undefined) {
+        res.send('You have no tasks assigned.');
+    } else {
+        // var geocodes = canv.task
+        // io.on('connection', function(socket) {
+        //     socket.emit('view-geocodes', geocodes);
+        // });
+
+        res.render("view-tasks", {
+            tasks: canv.task,
+            id: canv.ID.employeeID
+        });
+        // res.send(canv.task);
+    }
+
+    adminLogger.info(`/${req.params.id}/view-tasks - View Tasks`);
+});
+
+router.post('/view-task/:id', isAuthenticated, async (req: Request, res: Response) => {
+    const canv = await getManager()
+        .createQueryBuilder(Canvasser, "canvasser")
         .leftJoinAndSelect("canvasser._results", "results")
         .leftJoinAndSelect("canvasser._task", "task")
         .where("campaign._ID = :ID", { ID: req.params.id })
         .getOne();
 
-    if (res.status(200)){
+    if (res.status(200)) {
         if (canv === undefined) {
-            res.send('You have no tasks assigned.');
-        } else {
-            res.render("view-tasks", {
-                tasks: canv.task
-            });
-            // res.send(canv.task);
-        }
-        res.send("Available Date Updated.");
-    }
-    else{
-        res.send("Error updating available dates");
-    }
+            var index;
 
-    adminLogger.info(`/${req.params.id}/view-tasks - View Tasks`);
+            for (let i in canv.task){
+                if (canv.task[i].ID == req.body.taskID){
+                    index = Number(i);
+                }
+            }
+
+            res.render("view-task-detail", {
+                task: canv.task[index]
+            });
+        } else {
+            res.send('Error retreiving task '+req.params.id);
+        }
+    } else {
+        res.status(404).send("Details of Task " + req.params.id + " was not found");
+    }
 });
 
 export { router as canvasserRouter }
