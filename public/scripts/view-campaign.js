8f313c630460d7d3940bfbc56cd9965dc67149dc
klou <kahoulou318@gmail.com>
2018-11-29 10:34:46 -0500

    small view-campaign changes

--- public/scripts/view-campaign.js
+++ public/scripts/view-campaign.js
@@ -1,28 +1,3 @@
-// $(document).ready(function() {
-//   $(".toggle-accordion").on("click", function() {
-//     var accordionId = $(this).attr("accordion-id"),
-//     numPanelOpen = $(accordionId + ' .collapse.show').length;
-    
-//     console.log("NUMBER:" + $(accordionId + ' .collapse.show').length);
-//     console.log(numPanelOpen);
-//     $(this).toggleClass("active");
-    
-//     if (numPanelOpen == 0) {
-//       openAllPanels(accordionId);
-//     } else {
-//       closeAllPanels(accordionId);
-//     }
-//   })
-  
-//   openAllPanels = function(aId) {
-//     $(aId + ' .panel-collapse:not(".show")').collapse('show');
-//   }
-//   closeAllPanels = function(aId) {
-//     $(aId + ' .panel-collapse.show').collapse('hide');
-//   }
-  
-// });
-
 $('.closeAll').click(function(){
   $('.panel-collapse.show')
     .collapse('hide');
